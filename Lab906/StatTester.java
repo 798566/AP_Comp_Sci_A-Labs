ea52f9a68dde43865840e3491e4a91a9263e9185
798566 <42599884+798566@users.noreply.github.com>
2018-09-10 10:27:19 -0700

    Add files via upload

--- Lab906/StatTester.java
+++ Lab906/StatTester.java
@@ -3,26 +3,23 @@
  * StatTester finds loads an Array of int
  * and finds the sum, mean, median, and mode.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Sophia Raab 
+ * @910
  */
 
 // imports go here
 import java.util.Arrays;
 public class StatTester{
-    //  Instance variables
-    private int[] nums;
-    
-    // Constructor
-    public StatTester(){
-        
-    }
-    
-    
-    //  Methods
+    int[] nums;
+    nums = new int[100];
     public void loadArray(){
-    
+        for( int i = 0; i < nums.length; i++){
+            nums[i] = int (Math.random()*10 + 1);
+        }
+        int sum = 0
+        for(int 1 = 0; i < nums.length; i++){
+            sum = sum + nums[i];
+        }
+        System.out.println(sum);
     }
-    
-    
 }
